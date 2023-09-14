d52c517be28e058082f685f09923695a36034f26
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2023-09-13 23:46:59 -0300

    :sparkles: create about me section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { NavBar, Profile } from "../components/components";
 import "./home.css";
+import aboutMeImg from "../assets/aboutMe.webp";
 
 const Home = () => {
   return (
@@ -9,6 +10,32 @@ const Home = () => {
         <NavBar />
         <Profile />
       </header>
+
+      <section id="section">
+        <div id="aboutMe">
+          <img
+            src={aboutMeImg}
+            alt="imagem gerada por IA de um homem usando um computador"
+            className="aboutMeImg"
+          />
+
+          <div className="aboutMeContent">
+            <h2 className="aboutMeTitle">Sobre mim</h2>
+
+            <p className="aboutMeSubtitle">
+              Atualmente estou no 3º Período do curso de Sistemas para internet na
+              UNICAP - Universidade Católica De Pernambuco - e atuando em projetos
+              web como Desenvolvedor Frontend.
+              <span>
+                Desenvolvo páginas web modernas e resolvo problemas de usuários
+                usando as principais tecnologias do mercado atual. Busco sempre
+                a Alta performance , Responsividade, Acessibilidde, SEO,
+                Manutenção e a Melhor solução para o seu produto.
+              </span>
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
