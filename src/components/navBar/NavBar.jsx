88800336555443f1e1bc767f6f45135171b9d080
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2023-10-06 16:07:48 -0300

    :lipstick: style: add navbar responsive to mobile devices

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,7 +1,11 @@
 import "./navBar.css";
 import { Link } from "react-router-dom";
+import Hamburger from "hamburger-react";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <>
       <nav id="navBar">
@@ -16,6 +20,25 @@ const NavBar = () => {
             <Link>Experiência</Link>
             <Link>Contato</Link>
           </div>
+
+          <div className="menu" onClick={() => setIsOpen(!isOpen)}>
+            <Hamburger rounded />
+          </div>
+
+          <ul id={isOpen ? "menuNav" : "menuNavClose"}>
+            <a href="#">
+              <li className="menuLink">Sobre mim</li>
+            </a>
+            <a href="#">
+              <li className="menuLink">Projetos</li>
+            </a>
+            <a href="#">
+              <li className="menuLink">Experiência</li>
+            </a>
+            <a href="#">
+              <li className="menuLink">Contato</li>
+            </a>
+          </ul>
         </div>
       </nav>
     </>
